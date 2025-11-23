f24fc8bc3ee3075e53896816c635173c219074a8
Kenneth Johannes Fang <kennethfang1000@gmail.com>
2025-11-23 23:32:49 +0700

    localisation setup

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('preferred_language')->default('id');
+            $table->string('preferred_currency')->default('IDR');
             $table->rememberToken();
             $table->timestamps();
         });
