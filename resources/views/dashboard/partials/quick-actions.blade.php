bd3a4af4a1eb611d8afff612a1378ba70ce7ae35
Kenneth Johannes Fang <kennethfang1000@gmail.com>
2025-11-27 07:45:05 +0700

    dashboard action buttons ui fix

--- resources/views/dashboard/partials/quick-actions.blade.php
+++ resources/views/dashboard/partials/quick-actions.blade.php
@@ -1,19 +1,19 @@
 <div class="mb-6 px-4 sm:px-0">
-    <div class="flex items-center justify-between gap-4">
-        <div class="flex flex-wrap gap-4">
-            <x-secondary-button onclick="window.location='{{ route('expenses.index') }}'">
+    <div class="flex flex-wrap sm:flex-nowrap items-center justify-between gap-4">
+        <div class="gap-4 w-full sm:w-fit">
+            <x-secondary-button onclick="window.location='{{ route('expenses.index') }}'" class="w-full sm:w-fit">
                 <x-heroicon-o-eye class="w-5 h-5 mr-2" />
                 {{ __('messages.view_all_expenses') }}
             </x-secondary-button>
         </div>
 
-        <div class="flex items-center gap-4">
-            <x-secondary-button onclick="window.location='{{ route('budgets.create') }}'">
+        <div class="flex flex-wrap sm:flex-nowrap items-center gap-4">
+            <x-secondary-button onclick="window.location='{{ route('budgets.create') }}'" class="w-full sm:w-fit">
                 <x-heroicon-o-chart-bar class="w-5 h-5 mr-2" />
                 {{ __('messages.set_budget') }}
             </x-secondary-button>
 
-            <x-primary-button onclick="window.location='{{ route('expenses.create') }}'" class="inline-flex items-center">
+            <x-primary-button onclick="window.location='{{ route('expenses.create') }}'" class="inline-flex items-center w-full sm:w-fit">
                 <x-heroicon-o-plus class="w-5 h-5 mr-2" />
                 {{ __('messages.add_expense') }}
             </x-primary-button>
